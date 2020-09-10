d5020ec1d68f9b1a6341f5de42a29e206ee152fc
awc7182000 <69053773+awc7182000@users.noreply.github.com>
2020-09-10 14:00:57 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import styles from './Header.module.css';
+
+
+class Header extends Component {
+    render() {
+        return <div className={ styles.Header }></div>;
+    }
+}
+
+export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import styles from './Navigation.module.css';
+
+
+class Navigation extends Component {
+    render() {
+        return <div className={ styles.Navigation }></div>;
+    }
+}
+
+export default Navigation;
